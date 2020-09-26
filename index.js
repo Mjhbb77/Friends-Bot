2c3b23f0243ac55bfad71ac0f832837a3771e206
Mjhbb77 <65271559+Mjhbb77@users.noreply.github.com>
2020-09-26 04:40:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,28 @@ client.queue = new Map();
 
 client.once("ready", () =>
   console.log("Ready, Logged in as " + client.user.tag)
+);
+
+fs.readdir(__dirname + "/commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+    console.log("Loading Command: "+commandName)
+  });
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(client.config.prefix) || message.author.bot)
+    return;
+  const args = message.content.slice(client.config.prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  const command = client.commands.get(commandName);
+  if (!command) return;
+
+  command.run(client, message, args);
+});
+
+client.login(client.config.token);
